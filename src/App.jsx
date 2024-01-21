d6960e27558c572eec7869e827cc0e1d63219e2b
dizas9 <sazid.ice07@gmail.com>
2024-01-21 19:13:01 +0600

    Review 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import Header from "./Components/Header";
 import Home from "./Pages/HomePage/Home";
 import Footer from "./Components/Footer";
 
 function App() {
   const [mobileMode, setMobileMode] = useState(false);
+  const [initialRender, setInitialRender] = useState(true);
 
   useEffect(() => {
     function handleShowPage() {
@@ -17,21 +15,24 @@ function App() {
         setMobileMode(true);
       }
     }
-    // handleShowPage();
+
+    if (initialRender) {
+      handleShowPage();
+      setInitialRender(false);
+    }
 
     window.addEventListener("resize", handleShowPage);
 
     return () => {
       window.removeEventListener("resize", handleShowPage);
     };
-  }, []);
-
+  }, [initialRender]);
 
   return !mobileMode ? (
     <body className="h-fit w-screen flex flex-col leading-10">
       <Header />
       <Home />
-      <Footer/>
+      <Footer />
     </body>
   ) : (
     <div className="flex h-screen w-screen justify-center items-center bg-[red] bg-gradient-to-br">
